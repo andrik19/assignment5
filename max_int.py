f9f29549662122d9180aa81660f6d3d5dc18ba8f
Andri K <andrik19@ru.is>
2020-09-03 20:38:16 +0000

    Algorithm written for max_int

--- max_int.py
+++ max_int.py
@@ -6,3 +6,10 @@ while num_int > 0:
     num_int = int(input("Input a number: "))
 
 print("The maximum is", max_int)    # Do not change this line
+
+#1) Ask the user for a number
+#2) Find what number is  the largest with max()
+#3) Ask the user for a number again
+#   if the number is <= 0 go to #4
+#   if the number is > 0 got back to #2
+#4) End the loop and print the max number
